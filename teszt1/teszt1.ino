47cef43106ab23bdd1d4b409b3d62820be30883e
Reckfullz <erdei_benjamin@hotmail.com>
2015-12-08 15:46:28 +0100

    testing the keypad with a single code

---
+++ teszt1/teszt1.ino
@@ -0,0 +1,46 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Three columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = { 9, 8, 7, 6 };
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 5, 4, 3, 2 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+  digitalWrite(13, HIGH);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {
+    switch (key)
+    {
+      case '*':
+        digitalWrite(13, LOW);
+        break;
+      case '#':
+        digitalWrite(13, HIGH);
+        break;
+      default:
+        Serial.println(key);
+    }
+  }
+}
